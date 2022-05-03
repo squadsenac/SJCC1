86a82b474282668deac8038628abf00c8ff92754
squadsenac <104534251+squadsenac@users.noreply.github.com>
2022-05-03 18:36:31 -0300

    Optimização da parte de tags e outros detalhes.
    Falta resolver o bug nos links dinamicos da parte de organização.

--- js/behaviors_sjcc.js
+++ js/behaviors_sjcc.js
@@ -1,12 +1,17 @@
 
     let btn_lista = document.getElementById("Lista");
     let btn_mosaico = document.getElementById("Mosaico");
+    let btn_thumbs = document.getElementById("thumbs");
     let btn_prev = document.getElementById("link-prev");
     let bnt_add_tag = document.getElementById("plus");
-    let field_aviso = document.getElementById("aviso");
+    var field_aviso = document.getElementById("aviso_tags");
+    let tagsVars = [];
 
-    let countTag = 1;
+    // Contagem de Tags
+    var countTag = 1;
+    field_aviso.innerHTML = countTag-1 + " /8 tags adicionadas."
 
+    //Urls de acordo com o tipo de Organização(a revisar, funcionou na primeira pagina de tags, agora parou de funcionar)
     btn_lista.addEventListener('click', function(){
                 btn_prev.href = "./lista.html";
     });
@@ -15,29 +20,38 @@
                 btn_prev.href = "./mosaico.html";
     });
 
-    const criarTag = () => {
+    btn_thumbs.addEventListener('click', function(){
+                btn_prev.href = "./thumbs.html";
+    });
+
+    //Função para criar tags, contar o número de criadas e deletá-las apertendo o 'X'.
+    function criarTag(){
         
         if(countTag<=8){ 
             
-            let nome_tag = document.getElementById("busca").value;
+            let nome_tag = document.getElementById("busca-tags").value;
 
             let new_tag = document.createElement("button");
             new_tag.type = "button";
             new_tag.id = "tag" + countTag;
             new_tag.innerHTML = nome_tag;
-            new_tag.className = "nome";    
+            new_tag.className = "nome";
+            tagsVars[countTag -1] = nome_tag;
+                
 
             let self_del = document.createElement("img");
             self_del.className = "delete";
             self_del.src = "src/icons8-delete-64.png";
             self_del.onclick = function(){
                 new_tag.remove();
-                countTag--;
-                field_aviso.innerHTML = "";               
+                field_aviso.innerHTML = countTag -2 + " /8 tags adicionadas.";
+                tagsVars[countTag -1] = ""; //a revisar (remover item especifico de um array)
+                countTag--; 
             };
         
             document.getElementById("tags2").appendChild(new_tag);
             document.getElementById("tag" + countTag).appendChild(self_del);
+            field_aviso.innerHTML = countTag + " /8 tags adicionadas."
             countTag++;
             
         }else{
@@ -45,4 +59,28 @@
         };
 
     };
+
+    
+    //Função para gerar código com o botão a partir de Tags, datas e organização.
+    function gerarCodigo(){
+
+       /*  {
+            'apiKey': ""
+        } */
+
+        let videoURLs = []; 
+        let youtubeTags = [];
+
+            function compTags(tag) {
+                for(i = 0; i < compTags.length; i++){
+                    if(tagsVars[i] == youtubeTags[i]){
+                        //GetURL();
+                    }
+                }
+            }
+
+        tagsVars.forEach(compTags);
+
+        
+    }
 
